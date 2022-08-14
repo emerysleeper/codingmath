77d2899daea703c7833b8794d2e1183e500db50d
Eschaton-Futuro\Eschaton <emerysleeper@gmail.com>
2022-08-14 21:34:05 +0300

    Added "ship" that can be accelerated

--- Coding4 - Vectors and Velocity/script.js
+++ Coding4 - Vectors and Velocity/script.js
@@ -1,43 +1,84 @@
-// let v1 = vector.create(10, 5)
-// let v2 = vector.create(3, 4)
-// let v3 = v1.add(v2)
-
 window.onload = function() {
-    let canvas = document.querySelector('#canvas'),
-    context = canvas.getContext('2d'),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight,
-    p = particle.create(100, height, 0, -Math.PI/ 2)
-    accel = vector.create(0.01, -0.01)
-    // particles = []
-    // numParticles = 100
-
-    // for (var i = 0; i < numParticles; i+=1) {
-    //     particles.push(particle.create(width/2, height/2, Math.random() + 1, Math.random() * Math.PI * 2))
-    // }
+    let canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight,
+        ship = particle.create(width / 2, height/ 2, 0, 0),
+        thrust = vector.create(0, 0)
 
 
     update()
 
+    document.body.addEventListener("keydown", function(event) {
+        switch(event.keyCode) {
+            case 38:
+                thrust.setY(-0.1)
+                break
+            case 40:
+                thrust.setY(0.1)
+                break
+
+            case 37:
+                thrust.setX(-0.1)
+                break
+
+            case 39:
+                thrust.setX(0.1)
+                break
+
+            default:
+                break
+        }
+    })
+
+    document.body.addEventListener("keyup", function(event) {
+        switch(event.keyCode) {
+            case 38:
+                thrust.setY(0)
+                break
+            case 40:
+                thrust.setY(0)
+                break
+
+            case 37:
+                thrust.setX(0)
+                break
+
+            case 39:
+                thrust.setX(0)
+                break
+
+            default:
+                break
+        }
+    })
+
+
     function update() {
         context.clearRect(0, 0, width, height)
 
-        // for (var i = 0; i < numParticles; i+=1) {
-        //     let p = particles[i]
-        //     p.update()
-
-        //     context.beginPath()
-        //     context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 6, false)
-        //     context.fill()
-        // }
-
-        p.accelerate(accel)
-
-        p.update()
+        ship.accelerate(thrust)
+        ship.update()
 
         context.beginPath()
-        context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 6, false)
+        context.arc(ship.position.getX(), ship.position.getY(), 10, 0, Math.PI * 6, false)
         context.fill()
+
+        if(ship.position.getX() > width) {
+            ship.position.setX(0)
+        }
+
+        if(ship.position.getX() < 0) {
+            ship.position.setX(width)
+        }
+
+        if(ship.position.getY() > height) {
+            ship.position.setY(0)
+        }
+
+        if(ship.position.getY() < 0) {
+            ship.position.setY(height)
+        }
 
         requestAnimationFrame(update)
     }
